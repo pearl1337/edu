a72819b87e88c9d0d79bfaba077512a1b97bd1f8
pearl1337 <95040852+pearl1337@users.noreply.github.com>
2023-05-13 19:05:48 +0300

    setup eslint and add browserslist

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const counter = ref(0);
+
+function inc() {
+  counter.value += 1;
+}
+</script>
 <template>
   <div>
-    <p class="font-weight-black">
-      Cooming soon
+    <p v-if="1 > 2" class="font-weight-black" @click="inc">
+      Cooming soon {{ counter }}
     </p>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 
 const vuetify = createVuetify({
   components,
   directives,
-})
+});
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(vuetify)
-app.mount('#app')
+app.use(vuetify);
+app.mount("#app");
